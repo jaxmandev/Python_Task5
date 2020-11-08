a2746a60c758d79da653504a8e30bab2b3e03352
jaxmandev <39777721+jaxmandev@users.noreply.github.com>
2020-11-08 20:47:34 +0000

    Update pseudo script.py

--- script.py
+++ script.py
@@ -1,6 +1,8 @@
+# create parent class
 class Nucleobase:
     def __init__(self):
 
+        # create method to count A occurences
         def adenine(self, string):
             a_count = 0
             for i in string:
@@ -10,6 +12,7 @@ class Nucleobase:
                     continue
             return a_count
         
+        # create method to count C occurences
         def cytosine(self, string):
             c_count = 0
             for i in string:
@@ -19,6 +22,7 @@ class Nucleobase:
                     continue  
             return c_count 
 
+        # create method to count G occurences
         def guanine(self, string):
             g_count = 0
             for i in string:
@@ -28,6 +32,7 @@ class Nucleobase:
                     continue   
             return g_count      
 
+        # create method to count T occurences
         def thymine(self, string):
             t_count = 0
             for i in string:
@@ -37,9 +42,13 @@ class Nucleobase:
                     continue
             return t_count
 
+# test data
 dna_test = "AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC"
 
+# create an object or instance of the Nucleobase class
 parse_test = Nucleobase()
+
+# print the respective Nucleobase's counts
 print(parse_test.adenine(dna_test), parse_test.cytosine(dna_test), parse_test.guanine(dna_test), parse_test.thymine(dna_test))
 
 
